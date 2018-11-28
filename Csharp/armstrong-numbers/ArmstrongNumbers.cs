9e587ef351160d9a929819b73087b8d5ea6dcb3e
Gatis Jansons <bobsters@inbox.lv>
2018-11-28 23:17:36 +0000

    update

---
+++ Csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        IEnumerable<int> digits = number.ToString().Select(d => d - '0');
+			return digits.Aggregate(0,(s,i) => s + (int)Math.Pow(i,digits.Count())) == number;
+
+
+    }
+}
---
+++ Csharp/beer-song/BeerSong.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class BeerSong
+{
+
+	//"99 bottles of beer on the wall, 99 bottles of beer.\n" +
+    //"Take one down and pass it around, 98 bottles of beer on the wall.";
+    public static string Recite(int startBottles, int takeDown)
+    {
+		int b = startBottles;
+		string message = "";
+        for (int i=0;i<takeDown;i++)
+		{
+			message = message + $"{b} bottles of beer on the wall, {b} bottles of beer.\n" +
+            $"Take one down and pass it around, {b-1} bottles of beer on the wall.";
+			b=b-takeDown;
+
+		}
+		return message;
+		
+    }
+}
